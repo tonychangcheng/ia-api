728d9b0085679bbc78ed72a9c1df68e664bbc618
Zijun Yang <zijunhz@126.com>
2022-05-14 15:02:59 +0800

    chore: add static

--- goa_backend/settings.py
+++ goa_backend/settings.py
@@ -10,11 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 from .config import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 
 # Quick-start development settings - unsuitable for production
